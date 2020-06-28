20069c6ac69c357e4129060cd39953bcf72500a4
aniruthn <narayanan.aniruth@gmail.com>
2020-06-28 17:38:59 -0400

    added videoChange function

--- main.js
+++ main.js
@@ -8,3 +8,11 @@ function toggleNav() {
         nav.style.display = 'block';
     }
 }
+
+var videoStem = "https://www.youtube.com/embed/";
+
+function changeVid(URL, video) {
+    var videoURL = videoStem + URL;
+    var resourceVideo = document.getElementById(video);
+    resourceVideo.src = videoURL;
+}
