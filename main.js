2cbf01e7d676d2e63cb196b2bb1b66afcdc35a35
aniruthn <narayanan.aniruth@gmail.com>
2020-08-06 09:20:48 -0400

    started adding animations

--- main.js
+++ main.js
@@ -23,3 +23,45 @@ function changeSlides(pdf, title, link, button) {
     resourceButton.innerText = title;
     resourceLink.href = pdf;
 }
+
+//GSAP
+gsap.registerPlugin(MotionPathPlugin, ScrollTrigger, CSSRulePlugin);
+
+//homepage animations
+
+gsap.from(".hmpg-title-anim", {
+    duration: 2, 
+    xPercent: 50,
+    opacity: 0,
+    stagger: 0.5
+});
+
+const tl = gsap.timeline();
+
+const anim = tl.from(".hmpg-about-anim", {
+    xPercent: -20,
+    opacity: 0,
+    duration: 1,
+    stagger: 0.25
+});
+
+ScrollTrigger.create({
+    trigger: '.hmpg-about-anim',
+    animation: anim,
+    start: "top 70%",
+    end: "bottom bottom"
+});
+
+const anim2 = tl.from(".hmpg-vision-anim", {
+    xPercent: 20,
+    opacity: 0,
+    duration: 1,
+    stagger: 0.25
+});
+
+ScrollTrigger.create({
+    trigger: '.hmpg-vision-anim',
+    animation: anim2,
+    start: "top 70%",
+    end: "bottom bottom"
+});
