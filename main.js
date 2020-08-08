d9cf0be2aa99a9528df3630b42ef851d13a2db63
aniruthn <narayanan.aniruth@gmail.com>
2020-08-07 21:04:08 -0400

    added more animations

--- main.js
+++ main.js
@@ -23,3 +23,114 @@ function changeSlides(pdf, title, link, button) {
     resourceButton.innerText = title;
     resourceLink.href = pdf;
 }
+
+//GSAP
+gsap.registerPlugin(MotionPathPlugin, ScrollTrigger, CSSRulePlugin);
+
+const timelineVariable = gsap.timeline();
+
+const animationFromLeft = timelineVariable.from(".anim-from-left", {
+    xPercent: -20,
+    opacity: 0,
+    duration: 1,
+start: 20% top,
+    stagger: 0.5
+});
+
+ScrollTrigger.create({
+    trigger: '.anim-from-left',
+    animation: animationFromLeft
+});
+
+const timelineVariable2 = gsap.timeline();
+
+const animationFromRight = timelineVariable2.from(".anim-from-right", {
+    xPercent: 20,
+    opacity: 0,
+    duration: 1,
+start: 20% top,
+    stagger: 0.5
+});
+
+ScrollTrigger.create({
+    trigger: '.anim-from-right',
+    animation: animationFromRight
+});
+
+const timelineVariable3 = gsap.timeline();
+
+const animationFromDown = timelineVariable3.from(".anim-from-down", {
+    yPercent: 20,
+    opacity: 0,
+    delay: 1,
+    duration: 1,
+start: 20% top,
+    stagger: 0.5
+});
+
+ScrollTrigger.create({
+    trigger: '.anim-from-down',
+    animation: animationFromDown
+});
+
+/*const timelineVariable4 = gsap.timeline();
+
+const animationFromUp = timelineVariable4.from(".anim-from-up", {
+    yPercent: -20,
+    opacity: 0,
+    delay: 1,
+    duration: 1,
+start: 20% top,
+    stagger: 0.5
+});
+
+ScrollTrigger.create({
+    trigger: '.anim-from-up',
+    animation: animationFromUp
+});*/
+
+const timelineVariable5 = gsap.timeline();
+
+const animationFromLeft2 = timelineVariable5.from(".anim-from-left2", {
+    xPercent: -20,
+    opacity: 0,
+    duration: 1,
+start: 20% top,
+    stagger: 0.5
+});
+
+ScrollTrigger.create({
+    trigger: '.anim-from-left2',
+    animation: animationFromLeft2
+});
+
+const timelineVariable6 = gsap.timeline();
+
+const animationFromRight2 = timelineVariable6.from(".anim-from-right2", {
+    xPercent: 20,
+    opacity: 0,
+    duration: 1,
+start: 20% top,
+    stagger: 0.5
+});
+
+ScrollTrigger.create({
+    trigger: '.anim-from-right2',
+    animation: animationFromRight2
+});
+
+/*const timelineVariable7 = gsap.timeline();
+
+const animationFromDown2 = timelineVariable7.from(".anim-from-down2", {
+    yPercent: 20,
+    opacity: 0,
+    delay: 1,
+    duration: 1,
+start: 20% top,
+    stagger: 0.5
+});
+
+ScrollTrigger.create({
+    trigger: '.anim-from-down2',
+    animation: animationFromDown2
+});*/
